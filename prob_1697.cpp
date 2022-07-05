7b155374eaf80a16bfb4a2be43d1fdd2148d5aa8
HYGG2 <shng6815@naver.com>
2022-07-05 16:49:30 +0900

    1697 : 숨바꼭질(BFS)

--- prob_1697.cpp
+++ prob_1697.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
 bool visit[100001];
-int Min = 100001;
-void bfs(int N, int M, int cnt) {
-   cout<<N<<endl;
-   if (visit[N] || N > 100000 || N < 0) {
-      return;
-   }
-   if (N == M) {
-      Min = Min > cnt ? cnt : Min;
-      return;
-   }
+int ans;
 
-   visit[N] = true;
-   bfs(N - 1, M, cnt + 1);
-   bfs(N + 1, M, cnt + 1);
-   bfs(N * 2, M, cnt + 1);
-   visit[N] = false;
+queue<pair<int, int>> q;
+
+void bfs(int N, int M) {
+   while (!q.empty()) {
+      int data = q.front().first;
+      int depth = q.front().second;
+      q.pop();
+      if (data == M) {
+         ans = depth;
+         break;
+      }
+
+      if (data - 1 <= 100000 && data - 1 >= 0 && !visit[data - 1]) {
+         visit[data - 1] = true;
+         q.push({data - 1, depth + 1});
+      }
+      if (data + 1 <= 100000 && data + 1 >= 0 && !visit[data + 1]) {
+         visit[data + 1] = true;
+         q.push({data + 1, depth + 1});
+      }
+      if (data * 2 <= 100000 && data * 2 >= 0 && !visit[data * 2]) {
+         visit[data * 2] = true;
+         q.push({data * 2, depth + 1});
+      }
+   }
 }
 
 int main() {
    int N, M;
    cin >> N >> M;
+   q.push({N, 0});
+   visit[N] = true;
+   bfs(N, M);
 
-   bfs(N, M, 0);
-
-   cout << Min;
+   cout << ans;
 
    return 0;
 }
