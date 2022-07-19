49aaa71cef9adc9d95da643e49112c6ba5c559d4
HYGG2 <shng6815@naver.com>
2022-07-19 11:53:58 +0900

    11403 : 경로 찾기(FloydWarshall,)

---
+++ prob_11403.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int mat[101][101];
+
+void FloydWarshall(int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                if (mat[j][i] && mat[i][k]) {
+                    mat[j][k] = 1;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> mat[i][j];
+        }
+    }
+    FloydWarshall(N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
